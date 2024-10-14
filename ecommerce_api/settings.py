fb896f80032e6985b8d3501e4cc44f8861e47862
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-10-14 13:40:25 +0000

    add new file

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('django-insecure-zt-8+@66c+gabe*_kq07lcm@qza*_!y-ytm2ek(&#!y(be5%1$')
+SECRET_KEY = 'django-insecure-zt-8+@66c+gabe*_kq07lcm@qza*_!y-ytm2ek(&#!y(be5%1$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG =  True
 
 ALLOWED_HOSTS = ['.pythonanywhere.com']
 
